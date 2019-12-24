d1bd155c9066499bcf51c59d3d207534d3e5b4db
madawg <s.ajgou@gmail.com>
2019-12-24 20:14:16 +0100

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,6 +66,7 @@ module.exports = {
         icon: `content/assets/gatsby-icon.png`,
       },
     },
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
